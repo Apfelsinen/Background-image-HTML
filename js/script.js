b30232f91c78bfba9133181977bfe425b766716b
MacPika <enkhbaatar.azaa@icloud.com>
2021-04-23 11:16:38 +0800

    First Commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let number = 1.6;
+
+
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = "Alex Gun";
+
+const bool = true;
+
+const objMrAlex = {
+    name: "Alex",
+    age: 25,
+    isMarried: false
+};
+
+console.log(objMrAlex.name);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', [], {}];
+
+console.log(arr[5]);
+
+const result = confirm("Are you here?");
+console.log(result);
+const answer = prompt("Вам есть 18?");
+console.log(typeof(answer));
