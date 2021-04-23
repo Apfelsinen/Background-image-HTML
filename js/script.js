04dc0eb8960e84de56b1f9099efbec6db8cec06d
Pika <enkhbaatar.azaa@icloud.com>
2021-04-23 23:26:57 +0800

    Update script.js
    Task one

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 "use strict";
 
+const numberOfFilms = +prompt('How many movie did saw?', '');
 
-//check GitHub Desktop.
+const personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
+
+const a = prompt('One of last watched movie?', ''),
+      b = prompt( 'На сколько оцените?', ''),
+      c = prompt('Один из последних просмотреных фильмов?', ''),
+      d = prompt('Вопрос №4', '');
+
+personaMovieDB.movies[a] = b;
