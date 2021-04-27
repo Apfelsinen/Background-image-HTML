ac97ac3a2efd5faa9c0be70de47cbbf0fe6717e0
Pika <enkhbaatar.azaa@icloud.com>
2021-04-27 20:32:08 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,3 +24,13 @@ if (4 == 9) {
 } else {
     console.log('Error!');
 }
+
+const num = 50;
+
+if (num < 49) {
+    console.log('Error!');
+} else if (num > 100) {
+    console.log("Too Much!");
+} else {
+    console.log('Ok!');
+}
