e4842ff8bc5a1b8837b231c563abc9f904717790
Pika <enkhbaatar.azaa@icloud.com>
2021-05-03 09:26:42 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ const arr = [1, 2, 3];
 
 console.log(str.length); //4
 console.log(arr.length); //3
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test)); //12
+console.log(parseFloat(test)); //12.2
+
+function start() {
+
+}
