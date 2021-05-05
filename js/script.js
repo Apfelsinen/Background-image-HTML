de8e6cbecac7a58a8847c6989c78595db8b8d158
Pika <enkhbaatar.azaa@icloud.com>
2021-05-05 21:15:50 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,2 @@
 "use strict";
 //Lesson #18
-let numberOfFilms;
-function start() {
-    numberOfFilms = +promt('How many movies did you watched?', ''); //+для того чтобы был числовой тип данных
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +promt('How many movies did you watched?', '');
-    }
-}
-
-start();
